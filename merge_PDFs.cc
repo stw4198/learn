94f502c077c0d134f3dbf538a97969b118bca7af
Steve Wilson <stw4198@aol.com>
2021-07-08 14:03:44 +0100

    added hadd command to merge_PDFs

--- merge_PDFs.cc
+++ merge_PDFs.cc
@@ -26,7 +26,7 @@ void merge_PDFs(const char* sig){
   std::vector<std::string> line_values;
   std::ifstream theFile ("rates.csv");
   std::string line;
-  std::getline(theFile, line); //remove header
+  std::getline(theFile, line);
   
   while(std::getline(theFile, line))
   {
@@ -35,21 +35,18 @@ void merge_PDFs(const char* sig){
     while(std::getline(ss, line_value, ','))
     {
       line_values.push_back(line_value);
-      //printf("line_value = %s\n",line_value.c_str());
     }
   }
   
   for (int i = 0; i < line_values.size(); i = i + 2) {
     components.push_back(line_values[i]);
-    //printf("components[%i] = %s\n",i,line_values[i].c_str());
   }
   
   for (int i = 1; i < line_values.size(); i = i + 2) {
     rates.push_back(std::stod(line_values[i]));
-    //printf("rates[%i] = %f\n",i,std::stod(line_values[i]));
   }
   
-  if(strncmp(sig, "hartlepool",10)==0){
+  if(strncmp(sig, "hartlepool",strlen(sig))==0){
     signal.push_back("big");
     signal.push_back("small");
     background.push_back("singles");
@@ -58,21 +55,21 @@ void merge_PDFs(const char* sig){
     background.push_back("geo");
     background.push_back("world");
   }
-  else if(strncmp(sig, "heysham",10)==0){
+  else if(strncmp(sig, "heysham",strlen(sig))==0){
     signal.push_back("heysham");
     background.push_back("singles");
     background.push_back("n17");
     background.push_back("li9");
     background.push_back("heysham_bg");
   }
-  else if(strncmp(sig, "heysham2",10)==0){
+  else if(strncmp(sig, "heysham2",strlen(sig))==0){
     signal.push_back("heysham2");
     background.push_back("singles");
     background.push_back("n17");
     background.push_back("li9");
     background.push_back("heysham2_bg");
   }
-  else if(strncmp(sig, "torness",10)==0){
+  else if(strncmp(sig, "torness",strlen(sig))==0){
     signal.push_back("torness");
     background.push_back("singles");
     background.push_back("n17");
@@ -99,13 +96,8 @@ void merge_PDFs(const char* sig){
   
   printf("Signal rate = %e per second\nBackground rate = %e per second\n",signal_rate,background_rate);
   
-  int nbins = /*signal.size()**/200;
-  
-  //read in signal pdfs
   for(int i=0; i<components.size(); i++){
-  
-  //need to isolate signal and background
-    if(std::find(signal.begin(), signal.end(), components[i]) != signal.end()){
+      if(std::find(signal.begin(), signal.end(), components[i]) != signal.end()){
       printf("Scaling signal component %s\n",components[i].c_str());
       TFile *like = new TFile(Form("%s_pdfs.root",signal[i].c_str()));
       TH1D* n100 = (TH1D*)like->Get("n100");
@@ -152,18 +144,35 @@ void merge_PDFs(const char* sig){
   }
   
   //hadd signal and background components
+    
+  std::string sig_hadd = "hadd -f signal_pdfs.root";
+  std::string bg_hadd = "hadd -f background_pdfs.root";
 
+  for(int i=0;i<signal.size();i++){
+    sig_hadd += " ";
+    sig_hadd += signal[i];
+    sig_hadd += "_scaled_pdfs.root";
+  }
+  
+  for(int i=0;i<background.size();i++){
+    bg_hadd += " ";
+    bg_hadd += background[i];
+    bg_hadd += "_scaled_pdfs.root";
+  }
+ 
+ printf("\nRunning command:\n%s\n\n",sig_hadd.c_str());
+ system(sig_hadd.c_str());
+ printf("\nRunning command:\n%s\n\n",bg_hadd.c_str());
+ system(bg_hadd.c_str());
 }
 
 int main(int argc, char** argv){
 
   const char* sig = "hartlepool";
   if (argc > 1) {sig = argv[1];}
+  
+  printf("\n\n\nScaling and merging PDFs\n\n\nSignal = %s\n\n\n",sig);
 
-  //const char* sig = argv[1]; //input file (FRED)
-  //const char* outfile = argv[2]; //output file (likehoods)
   merge_PDFs(sig);
-  // have component type/rate as an input?
-  // need MC information for number of events simulated
 
 }
